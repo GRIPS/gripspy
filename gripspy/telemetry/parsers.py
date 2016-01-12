46b3b217abbe091e2777432fba2c8645c1581907
Albert Y. Shih <ayshih@gmail.com>
2016-01-12 16:59:25 +1300

    Added parser for raw card-cage event packets

--- gripspy/telemetry/parsers.py
+++ gripspy/telemetry/parsers.py
@@ -38,22 +38,25 @@ def parser(packet, filter_systemid=None, filter_tmtype=None):
         # Event packet
         if header['tmtype'] == 0xF3: 
             return ge_event(buf, header)
+        # Raw event packet
+        elif header['tmtype'] == 0xF1 or header['tmtype'] == 0xF2:
+            return ge_raw_event(buf, header)
 
 
 def ge_event(buf, out):
     """Assumes only one event in each event packet
     """
-    if ((out['systemid'] & 0xF0) != 0x80) or (out['tmtype'] != 0xF3):
-        raise InputError
+    if out['systemid'] & 0xF0 != 0x80 or out['tmtype'] != 0xF3:
+        raise ValueError
 
     index = INDEX_PAYLOAD
 
     out['detector'] = out['systemid'] & 0x0F
 
-    out['veto_lvgr'] = buf[index] & 1
-    out['veto_hvgr'] = buf[index] >> 1 & 1
-    out['veto_shield'] = buf[index] >> 2 & 1
-    out['veto_multiple'] = buf[index] >> 3 & 1
+    out['veto_lvgr'] = np.bool(buf[index] & 1)
+    out['veto_hvgr'] = np.bool(buf[index] >> 1 & 1)
+    out['veto_shield'] = np.bool(buf[index] >> 2 & 1)
+    out['veto_multiple'] = np.bool(buf[index] >> 3 & 1)
 
     time_lsb = buf[index] >> 7 | buf[index + 1] << 1 | buf[index + 2] << 9
     time_diff = (out['systime'] & ((1 << 17) - 1)) - time_lsb
@@ -74,7 +77,7 @@ def ge_event(buf, out):
     asiccha = np.zeros(512, np.uint16)
     has_conversion = np.zeros(512, np.bool)
     has_trigger = np.zeros(512, np.bool)
-    adc = np.zeros(512, np.uint16)
+    adc = np.zeros(512, np.int16)
     has_glitch = np.zeros(512, np.bool)
     delta_time = np.zeros(512, np.int8)
 
@@ -104,5 +107,42 @@ def ge_event(buf, out):
     out['adc'] = adc[:loc]
     out['has_glitch'] = has_glitch[:loc]
     out['delta_time'] = delta_time[:loc]
+
+    return out
+
+def ge_raw_event(buf, out):
+    """
+    """
+    if out['systemid'] & 0xF0 != 0x80 or (out['tmtype'] != 0xF1 and out['tmtype'] != 0xF2):
+        raise ValueError
+
+    out['detector'] = out['systemid'] & 0x0F
+    out['side'] = 'LV' if out['tmtype'] == 0xF1 else 'HV'
+
+    out['veto_multiple'] = np.bool(out['systime'] >> 47 & 1)
+    out['systime'] &= (1 << 48) - 1
+    out['event_time'] = out['systime']
+
+    adc = np.zeros(256, np.int16)
+    has_glitch = np.zeros(256, np.bool)
+    has_trigger = np.zeros(256, np.bool)
+    trigger_time = np.zeros(256, np.uint16)
+
+    index = INDEX_PAYLOAD
+
+    for i in range(32):
+        has_trigger[i*8:(i+1)*8] = np.array([buf[index] >> j & 1 for j in range(8)])
+        index += 1
+
+    for i in range(256):
+        adc[i] = buf[index] | (buf[index+1] & 0x7F) << 8
+        has_glitch[i] = buf[index+1] >> 7
+        trigger_time[i] = buf[index+2] | buf[index+3] << 8
+        index += 4
+
+    out['adc'] = adc
+    out['has_glitch'] = has_glitch
+    out['has_trigger'] = has_trigger
+    out['trigger_time'] = trigger_time
 
     return out
